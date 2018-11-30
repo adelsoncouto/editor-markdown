825cf34592edd3f42dc7855b6ca7ccacad8d6551
Adelson Silva Couto <adscouto@gmail.com>
2018-11-30 05:00:46 -0200

    ajustes no start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-./jre/bin/java Server 3000 $1
+# diretório onde está o server
+dir="$(dirname $(realpath $0))"
+
+# entro no diretório
+cd "$dir"
+
+# porta padrão
+porta="3000"
+
+# local do MD padrão
+md="$1"
+
+# Verifico se foi informado uma porta
+if [[ -n "$2" ]];then
+  porta="$1"
+  md="$2"
+fi
+
+# inicio o server
+./jre/bin/java Server "$porta" "$md"
+
